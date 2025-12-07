8704a3cc48a5974a33a914328912094fb786afc0
devchap8 <62908499+devchap8@users.noreply.github.com>
2025-12-07 08:06:22 -0500

    Implement playRound and findRoundWinner
    Commit README. Finish getPlayerChoice. Implement playRound, which
    gets player and computer input, finds the round winner with
    findRoundWinner, and increments score accordingly. Implement
    findRoundWinner, which uses a base list of rock, paper, scissors, and
    makes a new list to compare it to depending on player choice, which
    has the round outcome at the same indeces for each possible compChoice
    in the rpsList.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# rock-paper-scissors
+# Rock, Paper, Scissors
+
+Simple rock paper scizzors CLI program. Player chooses their option through a prompt in the browser, and the computer chooses randomly.
+The game is first to three wins, and can go up to five games. There is no error checking in the user prompt, so user must always provide correct input. 
+If player does not provide valid input, program defaults to rock.
--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let playerScore = 0;
+let compScore = 0;
+
 function getComputerChoice () {
     const randNum = Math.floor(Math.random() * 3);
     switch(randNum) {
@@ -6,13 +9,52 @@ function getComputerChoice () {
         case 1:
             return "paper";
         case 2:
-            return "scizzors";
+            return "scissors";
     }
     
 }
 
 function getPlayerChoice () {
-    const playerChoice = prompt("Rock, paper, or scizzors?").trim().toLowerCase();
-    if (playerChoice !== ("rock" || "paper" || "scizzors"))
+    const playerChoice = prompt("Rock, paper, or scissors?").trim().toLowerCase();
+    if (playerChoice !== "rock" && playerChoice !== "paper" && playerChoice !== "scissors") {
+        return "rock";
+    }
+    else {
+        return playerChoice;
+    }
 }
 
+function playRound () {
+    const compChoice = getComputerChoice();
+    const playerChoice = getPlayerChoice();
+    const outcome = findRoundWinner(playerChoice, compChoice);
+    if (outcome === "player") {
+        playerScore++;
+    }
+    else if (outcome === "computer") {
+        compScore++;
+    }
+}
+
+function findRoundWinner (playerChoice, compChoice) {
+    //compareList has the results for playerChoice vs compChoice at the index of rpsList
+    //e.g. if player chooses rock, rpsList[0] will be a tie because it is also rock, but 
+    //  rpsList[1] will be a loss because it is paper
+    //returns "player" if player wins, "computer" if computer wins, "tie" if tie
+    const rpsList = ["rock", "paper", "scissors"];
+    const compChoiceIndex = rpsList.indexOf(compChoice);
+    if (playerChoice === "rock") {
+        const compareList = ["tie", "computer", "player"];
+        return compareList[compChoiceIndex];
+    }
+    else if (playerChoice === "paper") {
+        const compareList = ["player", "tie", "computer"];
+        return compareList[compChoiceIndex];
+    }
+    else if (playerChoice === "scissors") {
+        const compareList = ["computer", "player", "tie"];
+        return compareList[compChoiceIndex];
+    }
+}
+
+playRound();
